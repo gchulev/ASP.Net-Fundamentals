80ec1e9ccd886d6220cba6a2aba72b754f0cbb73
gchulev <georgi.chulev@gmail.com>
2023-05-18 17:39:08 +0300

    Added task SumEvensInRange

---
+++ SumEvensInRange/Program.cs
@@ -0,0 +1,27 @@
+﻿while (true)
+{
+    string cmd = Console.ReadLine();
+    if (cmd == "show")
+    {
+        long result = SumAsync();
+        Console.WriteLine(result);
+        break;
+    }
+}
+
+static long SumAsync()
+{
+    return Task.Run(() =>
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }).Result;
+}
