ac1079e3a9fd052a41725402214b06a0c1e95fe5
gchulev <georgi.chulev@gmail.com>
2023-05-18 17:51:44 +0300

    Added SumEvensInBackground task

---
+++ SumEvensInBackground/Program.cs
@@ -0,0 +1,25 @@
+﻿long sum = 0;
+
+Task.Run(() =>
+{
+	for (int i = 0; i < 1000000000; i++)
+	{
+		if (i % 2 == 0)
+		{
+			sum += i;
+		}
+	}
+});
+
+while (true)
+{
+	string cmd = Console.ReadLine()!;
+	if (cmd == "show")
+	{
+        Console.WriteLine(sum);
+    }
+	if (cmd == "exit")
+	{
+		return;
+	}
+}
