a4b6f4f7374956d8e73bfe44fbbd3f83d3073480
gchulev <georgi.chulev@gmail.com>
2023-05-18 16:55:37 +0300

    Final Cleanup

---
+++ AsyncProcessing/Program.cs
@@ -0,0 +1,19 @@
+﻿int rangeStart = int.Parse(Console.ReadLine()!);
+int endRange = int.Parse(Console.ReadLine()!);
+
+Thread evenNums = new Thread(() => PrintEvenNumbers(rangeStart, endRange));
+
+evenNums.Start();
+evenNums.Join();
+Console.WriteLine("Thread finished work");
+
+static void PrintEvenNumbers(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
